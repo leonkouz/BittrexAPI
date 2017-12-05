8817c6a46101140ceff3095428b6ff7095f6794b
Leon <leon.kouz@hotmail.com>
2017-12-05 15:31:58 +1100

    Added MarketCurrency class to store data for GetCurrencies()

--- BittrexAPI/Structures/Currency.cs
+++ BittrexAPI/Structures/Currency.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace BittrexAPI.Structures
 {
-    public class Currency : IEnumerable
+    public class MarketCurrency : IEnumerable
     {
         private List<object> _currencyList = new List<object>();
 
@@ -21,10 +21,31 @@ namespace BittrexAPI.Structures
         }
 
         public string Currency { get; private set; }
+        public string CurrencyLong { get; private set; }
+        public float MinConfirmations { get; private set; }
+        public double TxFee { get; private set; }
+        public bool IsActive { get; private set; }
+        public string CoinType { get; private set; }
+        public string BaseAddress { get; private set; }
 
+        public MarketCurrency(string currency, string currencyLong, float minConfirmations, double txFee, bool isActive, string coinType, string baseAddress)
+        {
+            Currency = currency;
+            CurrencyLong = currencyLong;
+            MinConfirmations = minConfirmations;
+            TxFee = txFee;
+            IsActive = isActive;
+            CoinType = coinType;
+            BaseAddress = baseAddress;
 
-
-
-
+            //Adds all fields to the list to allow for implementation of IEnumerable
+            _currencyList.Add(Currency);
+            _currencyList.Add(CurrencyLong);
+            _currencyList.Add(MinConfirmations);
+            _currencyList.Add(TxFee);
+            _currencyList.Add(IsActive);
+            _currencyList.Add(CoinType);
+            _currencyList.Add(BaseAddress);
+        }
     }
 }
